0933c9e2abfd855abb8c9a29440cf5d26eedf131
JBH <xz7840@dgu.edu>
2024-02-04 02:50:50 +0900

    ✨ [FEAT] : CORS 설정 추가

--- blink/settings.py
+++ blink/settings.py
@@ -90,6 +90,7 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -147,6 +148,20 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
+]
+
+# CORS 설정
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1',
+    'http://127.0.0.1:3000',
+    'http://127.0.0.1:5173',
+    
+    'http://localhost',
+    'http://localhost:3000',
+    'http://localhost:5173',
 ]
 
 
