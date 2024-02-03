005cd797afb5b1c7d9486fcc36107939410bec7e
JBH <xz7840@dgu.edu>
2024-02-04 05:21:38 +0900

    ✨ [FEAT] : Kakao Social Login

--- user/oauth.py
+++ user/oauth.py
@@ -13,6 +13,7 @@ from dj_rest_auth.registration.views import SocialLoginView
 from allauth.socialaccount.models import SocialAccount
 from allauth.socialaccount.providers.google import views as google_view
 from allauth.socialaccount.providers.naver import views as naver_view
+from allauth.socialaccount.providers.kakao import views as kakao_view
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 
 
@@ -123,7 +124,7 @@ def google_callback(request):
     login(request, user)
 
     response = {
-        "messeage" : "login!",
+        "messeage" : "google login!",
         "access_token" : access_token,
         "refresh_token" : str(refresh)
     }
@@ -136,7 +137,7 @@ class GoogleLogin(SocialLoginView):
     client_class = OAuth2Client
 
 # 네어베 로그인
-NAVER_CALLBACK_URI = BASE_URL + 'api/v1/user/naver/login/callback/'
+NAVER_CALLBACK_URI = BASE_URL + 'api/v1/user/naver/callback/'
 
 def naver_login(request):
     print(NAVER_CALLBACK_URI)
@@ -220,14 +221,14 @@ def naver_callback(request):
         return JsonResponse({'err_msg': 'email exists but not social user'}, status=status.HTTP_400_BAD_REQUEST)
         
     user = User.objects.get(email=email)
+    
+    login(request, user)
 
     refresh = RefreshToken.for_user(user)
     access_token = str(refresh.access_token)
 
-    login(request, user)
-
     response = {
-        "messeage" : "login!",
+        "messeage" : "naver login!",
         "access_token" : access_token,
         "refresh_token" : str(refresh)
     }
@@ -238,4 +239,105 @@ def naver_callback(request):
 class NaverLogin(SocialLoginView):
     adapter_class = naver_view.NaverOAuth2Adapter
     callback_url = NAVER_CALLBACK_URI
+    client_class = OAuth2Client
+
+# 카카오 로그인
+KAKAO_CALLBACK_URI = BASE_URL + 'api/v1/user/kakao/callback/'
+
+def kakao_login(request):
+    client_id = os.environ.get("SOCIAL_AUTH_KAKAO_CLIENT_ID")
+    return redirect(f"https://kauth.kakao.com/oauth/authorize?client_id={client_id}&redirect_uri={KAKAO_CALLBACK_URI}&response_type=code&scope=account_email")
+
+def kakao_callback(request):
+    client_id = os.environ.get("SOCIAL_AUTH_KAKAO_CLIENT_ID")
+    code = request.GET.get("code")
+
+    # code로 access token 요청
+    token_request = requests.get(f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={client_id}&redirect_uri={KAKAO_CALLBACK_URI}&code={code}")
+    token_response_json = token_request.json()
+
+    # 에러 발생 시 중단
+    error = token_response_json.get("error", None)
+    if error is not None:
+        raise JSONDecodeError(error)
+
+    access_token = token_response_json.get("access_token")
+
+    # access token으로 카카오톡 프로필 요청
+    profile_request = requests.post(
+        "https://kapi.kakao.com/v2/user/me", headers={"Authorization": f"Bearer {access_token}"},
+    )
+    profile_json = profile_request.json()
+    
+    error = profile_json.get("error")
+    if error is not None:
+        raise JSONDecodeError(error)
+    
+    kakao_account = profile_json.get("kakao_account")
+    # print(kakao_account)
+    email = kakao_account.get("email", None) 
+    nickname = kakao_account.get("profile", {}).get("nickname", None)
+
+    try:
+        user = User.objects.get(email=email)
+        social_user = SocialAccount.objects.get(user=user)
+
+        if social_user.provider != 'kakao':
+            return JsonResponse({'err_msg': 'no matching social type'}, status=status.HTTP_400_BAD_REQUEST)
+
+        data = {'access_token': access_token, 'code': code}
+        accept = requests.post(
+            f"{BASE_URL}api/v1/user/kakao/login/finish/", data=data)
+        accept_status = accept.status_code
+
+        if accept_status != 200:
+            return JsonResponse({'err_msg': 'failed to signin'}, status=accept_status)
+        
+        accept_json = accept.json()
+        accept_json.pop('user', None)
+
+        if accept_status != 200:
+            return JsonResponse({"err_msg": "failed to signin"}, status=accept_status)
+    
+    except User.DoesNotExist:
+        # 전달받은 이메일로 기존에 가입된 유저가 없다면 -> 새로운 회원가입 & 해당 유저의 jwt 발급
+        data = {'access_token': access_token, 'code': code}
+        accept = requests.post(
+            f"{BASE_URL}api/v1/user/kakao/login/finish/", data=data
+        )
+        accept_status = accept.status_code
+        
+        if accept_status != 200:
+            return JsonResponse({'err_msg': 'failed to signup'}, status=accept_status)
+        
+        accept_json = accept.json()
+        accept_json.pop('user', None)
+        
+        user = User.objects.get(email=email)
+        user.nickname = nickname
+        user.save()
+
+        # return JsonResponse(accept_json)
+
+    except SocialAccount.DoesNotExist:
+        return JsonResponse({'err_msg': 'email exists but not social user'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    user = User.objects.get(email=email)
+
+    refresh = RefreshToken.for_user(user)
+    access_token = str(refresh.access_token)
+
+    login(request, user)
+
+    response = {
+        "messeage" : "kakao login!",
+        "access_token" : access_token,
+        "refresh_token" : str(refresh)
+    }
+
+    return JsonResponse(response)
+
+class KakaoLogin(SocialLoginView):
+    adapter_class = kakao_view.KakaoOAuth2Adapter
+    callback_url = KAKAO_CALLBACK_URI
     client_class = OAuth2Client
