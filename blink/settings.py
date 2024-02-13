95f0b5b9a7e0bf0981df8cab542edbb2269dff3e
lsjmc0224 <lsjmc0224@gmail.com>
2024-02-13 21:32:30 +0900

    :bug: FIX : settings.py의 STATIC_URL과 MEDIA_URL 정상화

--- blink/settings.py
+++ blink/settings.py
@@ -215,7 +215,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media files
@@ -226,7 +226,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# media 추가
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
